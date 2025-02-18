8e906bb98b540801fe4a5cf62024398308cf4bd6
FranciscoDanDan <franciscodaniel4477@gmail.com>
2025-02-18 23:14:18 +0000

    Adição de Densidade e PIB per capita.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,15 +6,19 @@ int main() {
     int area;
     float PIB;
     int pontos_turisticos;
+
+    float densidade;
+    float PIBpercapita;
+
     int opcao;
 
     do {
-        printf("\nCadastro de Cartas\n");
+        printf("Cadastro de Cartas\n");
 
         printf("País: ");
         scanf(" %49s", pais);
 
-        printf("População: ");
+        printf("População:\n");
         scanf("%d", &populacao);
 
         printf("Área: ");
@@ -26,13 +30,17 @@ int main() {
         printf("Pontos Turísticos: ");
         scanf("%d", &pontos_turisticos);
 
-        printf("\nPaís: %s\n - População: %d\n - Área: %d\n - PIB: %.2f\n - Pontos Turísticos: %d\n", 
-               pais, populacao, area, PIB, pontos_turisticos);
+        densidade = populacao / area;
+        PIBpercapita = PIB / populacao;
+
+        printf("\nPaís: %s\n - População: %d\n - Área: %d\n - PIB: %.2f\n - Pontos Turísticos: %d\n - Densidade Populacional: %2.f\n - PiB per capita: %2.f\n", 
+               pais, populacao, area, PIB, pontos_turisticos, densidade, PIBpercapita);
         
         printf("\nDeseja cadastrar outra carta? (1 - Sim, 0 - Não): ");
         scanf("%d", &opcao);
     } while (opcao = 1);
 
     printf("\nEncerrando o programa...\n");
+    
     return 0;
 }
