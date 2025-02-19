784f4452ba6789cfcb40c5b9877cb44b9b61c3f1
FranciscoDanDan <franciscodaniel4477@gmail.com>
2025-02-19 22:31:22 +0000

    Adição de Pib per capita e Densidade.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,23 +2,16 @@
 
 int main() {
     char pais[50];
-    int populacao;
-    int area;
-    float PIB;
-    int pontos_turisticos;
-
-    float densidade;
-    float PIBpercapita;
-
-    int opcao;
+    int populacao, area, pontos_turisticos, opcao;
+    float PIB, densidade, PIBpercapita;
 
     do {
-        printf("Cadastro de Cartas\n");
+        printf("\nCadastro de Cartas\n");
 
-        printf("País: ");
-        scanf(" %49s", pais);
+        printf("País:\n");
+        scanf(" %49[^\n]", pais); // Lê o nome do país com espaços
 
-        printf("População:\n");
+        printf("População: ");
         scanf("%d", &populacao);
 
         printf("Área: ");
@@ -30,15 +23,15 @@ int main() {
         printf("Pontos Turísticos: ");
         scanf("%d", &pontos_turisticos);
 
-        densidade = populacao / area;
-        PIBpercapita = PIB / populacao;
+        densidade = (area > 0) ? (float)populacao / area : 0;
+        PIBpercapita = (populacao > 0) ? PIB / populacao : 0;
 
-        printf("\nPaís: %s\n - População: %d\n - Área: %d\n - PIB: %.2f\n - Pontos Turísticos: %d\n - Densidade Populacional: %2.f\n - PiB per capita: %2.f\n", 
+        printf("\nPaís: %s\nPopulação: %d\nÁrea: %d km²\nPIB: %.2f\nPontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per capita: %.2f\n", 
                pais, populacao, area, PIB, pontos_turisticos, densidade, PIBpercapita);
         
         printf("\nDeseja cadastrar outra carta? (1 - Sim, 0 - Não): ");
         scanf("%d", &opcao);
-    } while (opcao = 1);
+    } while (opcao == 1);
 
     printf("\nEncerrando o programa...\n");
     
