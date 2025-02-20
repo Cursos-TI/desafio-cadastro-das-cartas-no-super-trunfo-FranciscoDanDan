c0987b9e92bb6082d31b3fdeefac00a85c7aa7a0
FranciscoDanDan <franciscodaniel4477@gmail.com>
2025-02-20 23:39:03 +0000

    Adição de Super Poder

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,38 +2,34 @@
 
 int main() {
     char pais[50];
-    int populacao, area, pontos_turisticos, opcao;
+    unsigned long int populacao;  // Alterado para suportar números maiores
+    int area, pontos_turisticos;
     float PIB, densidade, PIBpercapita;
 
-    do {
-        printf("\nCadastro de Cartas\n");
+    printf("Cadastro de Cartas\n");
 
-        printf("País:\n");
-        scanf(" %49[^\n]", pais); // Lê o nome do país com espaços
+    printf("País:\n");
+    scanf("%s", pais);
 
-        printf("População: ");
-        scanf("%d", &populacao);
+    printf("População:\n");
+    scanf("%lu", &populacao);  // Usando %lu para unsigned long int
 
-        printf("Área: ");
-        scanf("%d", &area);
-        
-        printf("PIB: ");
-        scanf("%f", &PIB);
-
-        printf("Pontos Turísticos: ");
-        scanf("%d", &pontos_turisticos);
-
-        densidade = (area > 0) ? (float)populacao / area : 0;
-        PIBpercapita = (populacao > 0) ? PIB / populacao : 0;
-
-        printf("\nPaís: %s\nPopulação: %d\nÁrea: %d km²\nPIB: %.2f\nPontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per capita: %.2f\n", 
-               pais, populacao, area, PIB, pontos_turisticos, densidade, PIBpercapita);
-        
-        printf("\nDeseja cadastrar outra carta? (1 - Sim, 0 - Não): ");
-        scanf("%d", &opcao);
-    } while (opcao == 1);
-
-    printf("\nEncerrando o programa...\n");
+    printf("Área (km²):\n");
+    scanf("%d", &area);
     
+    printf("PIB:\n");
+    scanf("%f", &PIB);
+
+    printf("Pontos Turísticos:\n");
+    scanf("%d", &pontos_turisticos);
+
+    // Cálculo da densidade populacional e PIB per capita
+    densidade = (area) ? (float)populacao / area : 0;
+    PIBpercapita = (populacao) ? PIB / populacao : 0;
+
+    // Exibição dos dados
+    printf("\nPaís: %s\nPopulação: %lu\nÁrea: %d km²\nPIB: %.2f\nPontos Turísticos: %d\nDensidade Populacional: %.2f hab/km²\nPIB per capita: %.2f\n", 
+           pais, populacao, area, PIB, pontos_turisticos, densidade, PIBpercapita);
+
     return 0;
 }
